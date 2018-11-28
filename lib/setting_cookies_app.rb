d028970e1512897a10e2e2266344cbb91ff23e78
Jay Ortega <jfortega35@gmail.com>
2018-11-28 15:22:06 -0500

    changed /countme path name

--- lib/setting_cookies_app.rb
+++ lib/setting_cookies_app.rb
@@ -5,7 +5,7 @@ class SettingCookiesApp
     @request = Rack::Request.new(env)
     case @request.path
       when "/" then Rack::Response.new(render("index.html.erb"))
-      when "/fml"
+      when "/countme"
         Rack::Response.new do |response|
           check = @request.cookies["count"].nil? ? 1 : @request.cookies["count"].to_i + 1
           response.set_cookie("count", check)
